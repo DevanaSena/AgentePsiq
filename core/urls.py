741b7f23cea397abdc22397b455adde62576ca44
DevanaSena <watssenna@gmail.com>
2025-10-07 02:22:51 -0300

    feat(consulta):criado o app, criacao da views e models e edicao do core

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
-    path('consultas/', include('consultas.urls'))
+    path('consultas/', include('consultas.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
